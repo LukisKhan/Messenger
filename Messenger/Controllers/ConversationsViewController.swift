3724f7968b040d7b3b31b74c23f0f57a68605b7a
Luat Pham <luat32@gmail.com>
2020-06-13 13:02:04 -0400

    Added views for Login and register

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Rave BizzDev on 6/13/20.
+//  Copyright © 2020 Rave BizzDev. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
